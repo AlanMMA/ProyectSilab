1cde36f242e8733ab46401ee16ecd6a62d0335df
Alan Morales <alanmauricioxd62@gmail.com>
2024-10-14 21:15:11 -0600

    Agregando pequeños arreglos al dashboard

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    x-data="{ darkMode: localStorage.getItem('theme') === 'dark' }" x-bind:class="darkMode ? 'dark' : 'light'">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
